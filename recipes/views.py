93de7eedaf7eaf1a8242408dc45a6fd880ba8756
Lucas Salles Arantes <lucas.sarantes@gmail.com>
2024-01-23 17:44:05 -0300

    First partial part with django + html

--- recipes/views.py
+++ recipes/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Lucas Salles',
     })
-
-def contact(request):
-    return render(request, 'me-apague/temp.html')
-
-def about(request):
-    return HttpResponse('ABOUT')
