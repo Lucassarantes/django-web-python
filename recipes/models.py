b9a910f4f45c661f93ce0aea2076364281ea8d74
Lucas Salles Arantes <lucas.sarantes@gmail.com>
2024-01-24 22:54:30 -0300

    Solving category 404 error

--- recipes/models.py
+++ recipes/models.py
@@ -3,20 +3,26 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
 
-class recipe(models.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=175)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
-    preparation_time_unity = models.CharField(max_length=65)
+    preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unity = models.CharField(max_length=65)
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.title
