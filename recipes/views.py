e6e377da67e315c7683fc55957289fe66ac0e007
Lucas Salles Arantes <lucas.sarantes@gmail.com>
2024-02-05 12:51:50 -0300

    Search view tests using tdd and refactor test file to each view test file.

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.db.models import Q
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 from django.http import Http404
@@ -31,4 +32,16 @@ def search(request):
     if not search_term:
         raise Http404()
     
-    return render(request, 'recipes/pages/search.html', {'page_title':f'Search for "{search_term}"'})
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+    
+    return render(request, 'recipes/pages/search.html', {
+        'page_title':f'Search for "{search_term}"',
+        'search_term':search_term,
+        'recipes': recipes
+    })
