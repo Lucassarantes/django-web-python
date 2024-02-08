1b671e15ac2b4461740ffb25b85feba7af3f575e
Lucas Salles Arantes <lucas.sarantes@gmail.com>
2024-02-08 10:38:27 -0300

    Testing the pagination

--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,11 @@ from django.http import Http404
 from utils.pagination import make_pagination
 import os
 
-PER_PAGES = os.environ.get('OER_PAGE', 6)
+PER_PAGE = os.environ.get('PER_PAGE', 6)
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/home.html', context= {
         'recipes': page_obj,
@@ -23,7 +23,7 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
